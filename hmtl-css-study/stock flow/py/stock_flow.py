29b21b303c8d652aa99fdd649471c7bc968c098a
Carlos-ed432 <erasmocarlos1104@gmail.com>
2026-08-27 13:12:56 -0300

    primeiro commit do projeto de gestão de estoques

---
+++ hmtl-css-study/stock flow/py/stock_flow.py
@@ -0,0 +1,10 @@
+produtos = [{"Nome":"Mouse Logitech","Preço":389,"Estoque": 31},{"Nome":"Monitor 32","Preço":675,"Estoque": 22},{"Nome":"Webcam Full HD","Preço":484,"Estoque": 11},{"Nome":"Teclado mecânico","Preço":322,"Estoque": 51},{"Nome":"Cadeira Gamer ergonômica","Preço":879,"Estoque": 6},{"Nome":"Headphone ANC","Preço":399,"Estoque": 20}]
+
+#total = 0
+
+#for i in produtos:
+   # print(f"O estoque de {i['Nome']} vale um total de {i["Estoque"]*i["Preço"]}  ")  # {i["Estoque"]= valor da chave Estoque X o valor da chave preço *i["Preço"]} 
+  #  v = i["Estoque"]*i["Preço"]# Guarda cada valor do laço for {i["Estoque"]*i["Preço"]}  
+ #   total+=v # soma cada valor e adiciona a total
+#print(f'Valor total do estoque da loja {total}')
+
